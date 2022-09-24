ddbc35a2a3e8d605ebfab3c23a3d92e30a86fc73
Charles Adilike <adilaiksgazzy@yahoo.com>
2022-09-24 10:58:33 -0700

    fixed typo

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DR, "static")]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
-    STATIC_ROOT = [os.path.join(BASE_DR, "static")]
+    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
 
